cc4f71e26efe14b6ca055b1d64f65f828ddfa267
JaennPB <jaenn.bucio@gmaill.com>
2022-08-23 12:43:54 -0500

    initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Heading, NativeBaseProvider, StatusBar } from "native-base";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { Provider } from "react-redux";
+
+import { store } from "./src/app/store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Provider store={store}>
+        <NavigationContainer>
+          <NativeBaseProvider>
+            <Heading>Hello world!</Heading>
+          </NativeBaseProvider>
+        </NavigationContainer>
+      </Provider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
